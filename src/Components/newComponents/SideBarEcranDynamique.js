445759de88bf6c4fc263c3151fb95415c9440f5a
MSX-R <msx.coach@gmail.com>
2024-10-05 00:57:45 +0200

    ajout heure de pairs

--- src/Components/newComponents/SideBarEcranDynamique.js
+++ src/Components/newComponents/SideBarEcranDynamique.js
@@ -42,7 +42,7 @@ function SideBarEcranDynamique() {
   };
 
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full gap-28">
       {/* HEADER2 A CREER */}
       <div name="HEADER 2" className="flex justify-between p-4 border-b border-gray-300 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)]">
         <div className="flex flex-col justify-end">
@@ -56,13 +56,13 @@ function SideBarEcranDynamique() {
       </div>
 
       {/* PAGE DYNAMIQUE LIE AU SIDE BAR MENU*/}
-      <div className="flex flex-col p-8 space-y-8 ">
-        <p className="text-3xl">Clients</p>
+      <div className="flex flex-col p-8 gap-8 ">
+        <p className="text-4xl font-bold">Clients</p>
         <div className="flex gap-8 flex-wrap ">
           <CardDataStats title={"RDV Bilan effectués"} total={47} precedent={40} rate={47 - 40} icon={<FaUser />} levelUp Percentage={false} />
           <CardDataStats title={"Clients convertis"} total={9} precedent={8} rate={9 - 8} icon={<FaUserPlus />} levelUp Percentage={false} />
         </div>
-        <p className="text-3xl">Ventes</p>
+        <p className="text-4xl font-bold">Ventes</p>
         <div className="flex gap-8 flex-wrap ">
           <CardDataStats title={"Séance Unique"} total={25} precedent={20} rate={25 - 20} icon={<MdEvent />} levelUp Percentage={false} />
           <CardDataStats title={"Pack de séances"} total={5} precedent={4} rate={5 - 4} icon={<GiArchiveRegister />} levelUp Percentage={false} />
