b835a5d6fc2a9f4ec6b6ffee353fc3ba04669af7
Lomenne <lomenne14@gmail.com>
2024-10-07 05:01:23 +0200

    Fix update

--- backend/server.js
+++ backend/server.js
@@ -163,6 +163,31 @@ app.get('/api/users/roles', verifyToken, async (req, res) => {
   }
 });
 
+// Route pour récupérer un utilisateur spécifique par son ID, accès restreint
+app.get('/api/users/:id', verifyToken, async (req, res) => {
+  const userId = req.params.id;
+
+  // Restreindre l'accès : soit l'utilisateur est admin, soit il récupère ses propres infos
+  if (req.user.role !== 1 && req.user.id !== parseInt(userId)) {
+    return res.status(403).json({ message: "Accès refusé." });
+  }
+
+  const sql = 'SELECT id, nom, prenom, email, telephone, adresse1, adresse2, cp, ville, pays, naissance, contact_urgence, sexe, nb_enfant, role_id, date_creation FROM User WHERE id = ?';
+
+  try {
+    const [results] = await pool.query(sql, [userId]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Utilisateur non trouvé." });
+    }
+
+    res.status(200).json(results[0]); // Retourner l'utilisateur trouvé
+  } catch (err) {
+    console.error("Erreur lors de la récupération de l'utilisateur :", err);
+    res.status(500).json({ message: "Erreur lors de la récupération de l'utilisateur." });
+  }
+});
+
 // Route pour supprimer un utilisateur spécifique
 app.delete('/api/users/:id', verifyToken, verifyAdmin, async (req, res) => {
   const userId = req.params.id;
@@ -184,9 +209,9 @@ app.delete('/api/users/:id', verifyToken, verifyAdmin, async (req, res) => {
 });
 
 // Route pour mettre à jour un utilisateur spécifique
-app.put('/api/users/:id', verifyToken, async (req, res) => {
+app.put('/api/users/:id', verifyToken, verifyAdmin, async (req, res) => {
   const userId = req.params.id;
-  const { nom, prenom, email, telephone, adresse1, adresse2, cp, ville, pays, naissance, contactUrgence, sexe, nbEnfant, role_id } = req.body;
+  const { nom, prenom, email, telephone, adresse1, adresse2, cp, ville, pays, naissance, contactUrgence, sexe, nbEnfant } = req.body;
 
   // Construire la requête SQL pour mettre à jour l'utilisateur
   const sql = `
@@ -204,14 +229,13 @@ app.put('/api/users/:id', verifyToken, async (req, res) => {
       naissance = ?, 
       contact_urgence = ?, 
       sexe = ?, 
-      nb_enfant = ?, 
-      role_id = ? 
+      nb_enfant = ?
     WHERE id = ?
   `;
 
   try {
     const [results] = await pool.query(sql, [
-      nom, prenom, email, telephone, adresse1, adresse2, cp, ville, pays, naissance, contactUrgence, sexe, nbEnfant, role_id, userId
+      nom, prenom, email, telephone, adresse1, adresse2, cp, ville, pays, naissance, contactUrgence, sexe, nbEnfant, userId
     ]);
 
     if (results.affectedRows === 0) {
