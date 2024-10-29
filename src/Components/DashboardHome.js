1b03fb7e708fb3bd0de21d80b5a450b23f14124f
MSX-R <msx.coach@gmail.com>
2024-10-29 02:58:50 +0100

    correction bouron dasheboard

--- src/Components/DashboardHome.js
+++ src/Components/DashboardHome.js
@@ -5,6 +5,17 @@ import { FaUsers, FaChartLine, FaClipboardList, FaFileInvoice, FaStopwatch } fro
 
 const DashboardHome = () => {
   const { loggedUser } = useAuth(); // Utilisation de useAuth() pour obtenir loggedUser
+  console.log(loggedUser);
+
+  // Fonction pour vérifier si l'utilisateur a un rôle d'admin
+  const isAdmin = () => {
+    return loggedUser && loggedUser.role_id === 1;
+  };
+  // Utilisation de la fonction pour déclencher une action si l'utilisateur est admin
+  if (isAdmin()) {
+    console.log("L'utilisateur est un administrateur.");
+    // Placez ici toute action que vous voulez déclencher pour les administrateurs
+  }
 
   return (
     <>
@@ -13,45 +24,66 @@ const DashboardHome = () => {
         <h1 className="text-3xl font-bold mb-4">Bonjour, {loggedUser ? loggedUser.prenom : "Coach"} !</h1>
         <p className="text-lg text-gray-700 mb-8">Bienvenue sur votre tableau de bord de coaching. Vous trouverez ici tous les outils nécessaires pour gérer vos clients, suivre leur progression, et faciliter votre travail quotidien.</p>
 
-{/* BOUTONS DASHBOARD */}
+        {/* BOUTONS DASHBOARD */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Raccourci vers la Liste des Clients */}
-          <Link to="/dashboard/liste-clients" className="p-6 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition">
-            <div className="flex items-center">
-              <FaUsers size={24} className="mr-4" />
-              <span className="font-semibold text-xl">Gérer les Clients</span>
-            </div>
-          </Link>
-
-          {/* Raccourci vers Tableau des Statistiques */}
-          <Link to="/dashboard/tableau-des-stats" className="p-6 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition">
-            <div className="flex items-center">
-              <FaChartLine size={24} className="mr-4" />
-              <span className="font-semibold text-xl">Tableau des Statistiques</span>
-            </div>
-          </Link>
-
-          {/* Raccourci vers Formulaire de Devis */}
-          <Link to="/dashboard/formulaire-devis" className="p-6 bg-yellow-500 text-white rounded-lg shadow-md hover:bg-yellow-600 transition">
-            <div className="flex items-center">
-              <FaFileInvoice size={24} className="mr-4" />
-              <span className="font-semibold text-xl">Créer un Devis</span>
-            </div>
-          </Link>
-
-          {/* Raccourci vers Chronomètres Tabata */}
-          <Link to="/dashboard/tabata-chrono" className="p-6 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 transition">
-            <div className="flex items-center">
-              <FaStopwatch size={24} className="mr-4" />
-              <span className="font-semibold text-xl">Tabata Chrono</span>
-            </div>
-          </Link>
-
           {/* Raccourci vers Offres de Coaching */}
           <Link to="/dashboard/offres-coachings" className="p-6 bg-teal-500 text-white rounded-lg shadow-md hover:bg-teal-600 transition">
             <div className="flex items-center">
               <FaClipboardList size={24} className="mr-4" />
               <span className="font-semibold text-xl">Offres de Coaching</span>
+            </div>
+          </Link>
+
+          {/* Raccourci vers la Liste des Clients */}
+          {isAdmin() && (
+            // je veux que si l loggedUser.role_id === 1 alors on affiche le link ci dessous sinon
+            <Link to="/dashboard/liste-clients" className="p-6 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition">
+              <div className="flex items-center">
+                <FaUsers size={24} className="mr-4" />
+                <span className="font-semibold text-xl">Gérer les Clients</span>
+              </div>
+            </Link>
+          )}
+
+ {/* Raccourci vers Formulaire de Devis */}
+            <Link to="/dashboardquestionnaire-entretien" className="p-6 bg-yellow-500 text-white rounded-lg shadow-md hover:bg-yellow-600 transition">
+              <div className="flex items-center">
+                <FaFileInvoice size={24} className="mr-4" />
+                <span className="font-semibold text-xl">Questionnaire-Entretien </span>
+              </div>
+            </Link>
+
+          {/* Raccourci vers Tableau des Statistiques */}
+          <Link to="/dashboard/tableau-des-stats" className="p-6 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition">
+            <div className="flex items-center">
+              <FaChartLine size={24} className="mr-4" />
+              <span className="font-semibold text-xl">Données Client | Performances RM10</span>
+            </div>
+          </Link>
+
+          {/* Raccourci vers Formulaire de Devis */}
+          {isAdmin() && (
+            <Link to="/dashboard/formulaire-devis" className="p-6 bg-yellow-500 text-white rounded-lg shadow-md hover:bg-yellow-600 transition">
+              <div className="flex items-center">
+                <FaFileInvoice size={24} className="mr-4" />
+                <span className="font-semibold text-xl">Créer un Devis</span>
+              </div>
+            </Link>
+          )}
+
+          {/* Raccourci vers Chronomètres Tabata */}
+          <Link to="/dashboard/tabata-chrono" className="p-6 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 transition">
+            <div className="flex items-center">
+              <FaStopwatch size={24} className="mr-4" />
+              <span className="font-semibold text-xl">Outils | Chronomètres</span>
+            </div>
+          </Link>
+
+          {/* Raccourci vers Tests Cardio */}
+          <Link to="/dashboard/vma-tapis" className="p-6 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 transition">
+            <div className="flex items-center">
+              <FaStopwatch size={24} className="mr-4" />
+              <span className="font-semibold text-xl">Outils | Tests Cardio</span>
             </div>
           </Link>
         </div>
